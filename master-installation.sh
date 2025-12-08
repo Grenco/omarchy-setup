64b897994ef7da5c243f0dfb9483d16e8a7d3ebf
Grenco <graemesmyrl@gmail.com>
2025-12-08 09:48:38 +0000

    Add script to override hyprland keybindings

--- master-installation.sh
+++ master-installation.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 "$SCRIPT_DIR/install-tmux.sh"
 "$SCRIPT_DIR/install-stow.sh"
 "$SCRIPT_DIR/install-yazi.sh"
+"$SCRIPT_DIR/install-hyprland-overrides.sh"
 "$SCRIPT_DIR/install-dotfiles.sh"
 "$SCRIPT_DIR/install-kanata.sh"
 
